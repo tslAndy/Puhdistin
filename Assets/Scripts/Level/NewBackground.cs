c7719eecffa7a94e8c7130522217d006b8c4d94b
Andy <andy.tsl@pm.me>
2023-02-06 10:18:38 +0200

    Remake background script.

---
+++ Assets/Scripts/Level/NewBackground.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class NewBackground : MonoBehaviour
+{
+    [SerializeField]
+    private BackLayer[] backLayers;
+
+
+
+    void Start()
+    {
+        foreach (BackLayer backLayer in backLayers)
+        {
+            backLayer.images = new GameObject[3]; 
+
+            GameObject layer = new GameObject(backLayer.image.name + "_layers");
+            layer.transform.SetParent(transform);
+
+            backLayer.width = backLayer.image.GetComponent<SpriteRenderer>().bounds.size.x - 0.015f;
+            for (int i = 0; i < 3; i++)
+            {
+                GameObject image = Instantiate(backLayer.image, transform);
+                image.transform.SetParent(layer.transform);
+                image.transform.position += Vector3.right * backLayer.width * i;
+                backLayer.images[i] = image;
+            }
+
+            Destroy(backLayer.image);
+        }
+    }
+
+    void Update()
+    {
+        foreach (BackLayer backLayer in backLayers)
+        {
+            float dx = backLayer.speed * Time.deltaTime;
+            foreach (GameObject image in backLayer.images)
+            {
+                image.transform.position += Vector3.left * dx;
+
+         
+                if (image.transform.position.x + backLayer.width / 2 < 
+                    Camera.main.ViewportToWorldPoint(Vector3.zero).x)
+                {
+                    image.transform.position += Vector3.right * backLayer.width * 3;
+                }
+            }
+        }
+    }
+}
+
+[Serializable]
+public class BackLayer
+{
+    public GameObject image;
+    public float speed;
+
+    [NonSerialized]
+    public GameObject[] images;
+
+    [NonSerialized]
+    public float width;
+}
